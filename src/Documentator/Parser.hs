3acc4011f00401776932ec535696b36b64dd5c51
meditans <meditans@gmail.com>
2016-07-23 11:11:17 +0200

    Removed unuseful functions

--- src/Documentator/Parser.hs
+++ src/Documentator/Parser.hs
@@ -30,36 +30,17 @@ pRaw e = g e >>= mapM_ (\a -> print a >> putStrLn "\n")
 p :: (Pretty a) => Extractor [a] -> IO ()
 p e = g e >>= mapM_ (putStrLn . prettyPrint)
 
-myParseOnlyDeclarationsAndModuleName :: FilePath -> IO (ParseResult (ModuleName, [Decl]))
-myParseOnlyDeclarationsAndModuleName f = (fmap . fmap) extractDecls (myParseOnlyModule f)
-  where
-    extractDecls (Module _ mn _ _ _ _ ds) = (mn, ds)
-
-prettyPrintDeclarations f = do
-  ParseOk (_, ds) <- myParseOnlyDeclarationsAndModuleName f
-  mapM_ (\d -> print d >> putStrLn "\n") ds
-
-prettyList :: (Show a, Foldable t) => t a -> IO ()
-prettyList = mapM_ (\d -> print d >> putStrLn "\n")
-
--- We are mainly interested in the type signatures for now
-typeSignatures :: FilePath -> IO (ParseResult [Decl])
-typeSignatures f = (fmap . fmap) extractSignatures (myParseOnlyDeclarationsAndModuleName f)
-  where
-    extractSignatures (_, ds) = filter isTypeSig ds
-    isTypeSig (TypeSig _ _ _) = True
-    isTypeSig _               = False
-
-typeSignaturesExtractor :: Extractor [Decl]
-typeSignaturesExtractor = filter isTypeSig . declarations
-  where
-
 isTypeSig :: Decl -> Bool
 isTypeSig (TypeSig _ _ _) = True
 isTypeSig _               = False
 
-declarations :: Module -> [Decl]
-declarations (Module _ _ _ _ _ _ ds) = ds
+------------------------------------------------------------- Extractors
+
+typeSignaturesExtractor :: Extractor [Decl]
+typeSignaturesExtractor = filter isTypeSig . declarations
+  where
+    declarations :: Module -> [Decl]
+    declarations (Module _ _ _ _ _ _ ds) = ds
 
 typesExtractor :: Extractor [Type]
 typesExtractor = map getType . filter isTypeSig . typeSignaturesExtractor
