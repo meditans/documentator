dd85bada46ee612b7b65e28dd462346df81c29ce
Arguggi <git@arguggi.co.uk>
2016-07-24 10:08:28 +0200

    Get example filepath from cabal

--- src/Report/Main.hs
+++ src/Report/Main.hs
@@ -11,25 +11,28 @@ import Documentator.Types
 import Language.Haskell.Exts.Annotated
 import Data.String
 import Documentator.Parser
+import Documentator.Utils      (lensFileExample)
 
 
 main :: IO ()
-main = do mod <- myParse "/Users/luca/Zurihac/hakyll/src/Hakyll/Web/Template/Internal.hs" 
-          let topUsedTypes = typeUsages mod
-          report <- pure $ generateReport topUsedTypes
-          IO.writeFile "/tmp/report.html" report
-          putStrLn "generated /tmp/report.html"
-          openBrowser "file:///tmp/report.html"
-          return ()
+main = do
+  filePath <- lensFileExample
+  mod <- myParse filePath
+  let topUsedTypes = typeUsages mod
+  report <- pure $ generateReport topUsedTypes
+  IO.writeFile "/tmp/report.html" report
+  putStrLn "generated /tmp/report.html"
+  openBrowser "file:///tmp/report.html"
+  return ()
 
 generateReport :: TopUsedTypes -> T.Text
 generateReport = renderText . html
 
-type TopUsedTypes = [(Located Type, Int)] 
+type TopUsedTypes = [(Located Type, Int)]
 
 html :: TopUsedTypes -> Html ()
-html topUsedTypes = h1_ "Automatic DOC generator" <> 
-  h2_ "Top used types" 
+html topUsedTypes = h1_ "Automatic DOC generator" <>
+  h2_ "Top used types"
     <> table_ ( tr_ (th_ "Type Name" <> th_ "N. of Usages") <> rows)
   where rows = mconcat $ map row topUsedTypes
 
@@ -38,8 +41,3 @@ row :: (Located Type, Int) -> Html ()
 row (t, num) = tr_ (td_ firstColumn <> td_ secondColumn)
     where firstColumn = fromString (prettyPrint (fmap fst t))
           secondColumn = fromString $ show num
-        
-
-
-          
-      
